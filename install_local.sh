202aa936d06a1dd13e8ff00b4696604673992e1d
Rich J <rjarow@gmail.com>
2017-11-13 19:09:16 -0500

    docker-py confusion
    https://github.com/ansible/ansible/issues/22993

--- install_local.sh
+++ install_local.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 # Let's go!
+### Location to install doowop ( defaults to /opt/doowop )
+INSTALLDIR=/opt/doowop
 
-cd ~
-if [ ! -f /usr/bin/git ];
-then
-    sudo apt-get install git -y
-fi
+### Pre-Requisites - We need latest version of ansible from PPA
+sudo apt-get install git -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt install ansible -y
 
-if [ ! -f /usr/bin/ansible ];
-then
-    sudo apt install software-properties-common
-    sudo add-apt-repository ppa:ansible/ansible
-    sudo apt-get update
-    sudo apt install ansible
-fi
 
-if [ ! -d ~/.doowop ];
+
+if [ ! -d ${INSTALLDIR} ];
 then
-    git clone -b dev https://github.com/rjarow/doowop.git ~/.doowop
-    cd ~/.doowop
+    git clone -b dev https://github.com/rjarow/doowop.git $INSTALLDIR
+    cd ${INSTALLDIR}
     chmod +x bootstrap_local.yml
     sudo ansible-playbook bootstrap_local.yml
 else
-    cd ~/.doowop
+    cd ${INSTALLDIR}
     git pull
     chmod +x bootstrap_local.yml
     sudo ansible-playbook bootstrap_local.yml
